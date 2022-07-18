6e9fed81dee816c669a3875f656b08bfd8060322
Kengi Ortega <102864382+kortega90@users.noreply.github.com>
2022-07-18 11:05:03 -0300

    Update index.tsx

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import './styless.css';
 function Header() {
     return (
         <header>
-        <div class="dsmeta-logo-container">
+        <div className="dsmeta-logo-container">
             <img src={logo} alt="DSMeta" />
             <h1>DSMeta</h1>
             <p>
